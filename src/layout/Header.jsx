164b7e78674bf2c373c347ad45f35c1b330af032
gahramanova <narminhikmat@gmail.com>
2024-12-05 14:48:18 +0100

    Header page v0.1.2

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -20,6 +20,7 @@ const Header = () => {
             <li><Link to="/movies" className="nav-link px-2 text-light">Movies</Link></li>
             <li><Link to="/support" className="nav-link px-2 text-light">Support</Link></li>
             <li><Link to="/subscriptions" className="nav-link px-2 text-light">Subscription</Link></li>
+
           </ul>
           <div className="col-12 col-sm-6 col-md-3 text-end">
             <button type="button" className="btn me-2"><IoIosSearch style={{width: "30px", height:"30px"}}/></button>
